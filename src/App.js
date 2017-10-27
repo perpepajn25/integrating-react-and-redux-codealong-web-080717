a246817ab50e2b6fdc9e813590ca5476bf62711f
Natalie Perpepaj <perpepajn25@gmail.com>
2017-10-27 09:51:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import changeCount from './reducers/changeCount';
+import createStore from './createStore';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App" >
-        Counter Component here.
-      </div>
-    );
-  }
-}
+const store = createStore(changeCount);
 
-export default App;
+export function render() {
+ ReactDOM.render(
+   <App store={store} />,
+   document.getElementById('root')
+ );
+};
+
+store.dispatch({ type: '@@INIT' });
